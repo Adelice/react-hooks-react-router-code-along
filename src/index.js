9c7fa3355993e6cb34ed7560f8acece5be86f926
Adelice <nancytoniw@gmail.com>
2023-09-04 16:54:56 +0100

    add

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Login from "./Login";
+import About from "./About";
+import Home from "./Home";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
 
-function App() {
-  return <Home />;
-}
+ReactDOM.render(
+<BrowserRouter>
+  <Switch>
+  
+    <Route path="/about">
+      <About />
+    </Route>
+    <Route path="/login">
+      <Login />
+    </Route>
+    <Route exact path="/">
+      <Home />
+    </Route>
+  </Switch>
+  </BrowserRouter>,
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+document.getElementById("root"));
